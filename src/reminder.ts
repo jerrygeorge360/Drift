31b83bea9447620fbf9d454700ecaec1d7a7fbf0
jerry george <jerrygithub360@gmail.com>
2025-10-13 08:19:42 +0100

    todos

---
+++ src/reminder.ts
@@ -0,0 +1,8 @@
+// TODO : add authentication to webhook
+// TODO : add bundler provider to delegation
+// TODO : add admin,user and bot roles
+// TODO : add llm service to the bot
+// TODO : integrate hyperledger for tracking on chain activities
+// TODO : integrate price oracle
+// TODO : create the rules and service to trigger the webhook
+// TODO : Integrate backend **contract service** → read/write rules & delegation on-chain.
