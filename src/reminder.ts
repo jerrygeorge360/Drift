133e37c10bbc1091a739b7853fceed6706f88e0c
jerry george <jerrygithub360@gmail.com>
2025-10-13 08:25:55 +0100

    update

--- src/reminder.ts
+++ src/reminder.ts
@@ -1,8 +1,9 @@
-// TODO : add authentication to webhook
-// TODO : add bundler provider to delegation
-// TODO : add admin,user and bot roles
-// TODO : add llm service to the bot
-// TODO : integrate hyperledger for tracking on chain activities
-// TODO : integrate price oracle
-// TODO : create the rules and service to trigger the webhook
-// TODO : Integrate backend **contract service** → read/write rules & delegation on-chain.
+// TODO : add authentication to webhook(1)
+// TODO : add admin,user and bot roles(2)
+// TODO : add bundler provider to delegation(3)
+// TODO : add llm service to the bot(4)
+// TODO : Integrate backend **contract service** → read/write rules & delegation on-chain.(5)
+// TODO : integrate price oracle(6)
+// TODO : integrate hyperledger for tracking on chain activities(7)
+// TODO : create the rules and service to trigger the webhook(8)
+
