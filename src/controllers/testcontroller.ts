de510b81fd74b822063080c9c77082287b5b6939
jerry george <jerrygithub360@gmail.com>
2025-11-28 18:49:00 +0100

    update

--- src/controllers/testcontroller.ts
+++ src/controllers/testcontroller.ts
@@ -9,8 +9,8 @@ export const redeemDelegationController = async (req: Request, res: Response): P
                 botAddress: "0x1235aC2B678202802b5071a7AadF7efe0E172d0E",
                 tokenIn: "0xf817257fed379853cDe0fa4F97AB987181B1E5Ea",   // USDC
                 tokenOut: "0x88b8E2161DEDC77EF4ab7585569D2415a1C1055D", // USDT
-                amountOut: BigInt("1000000000000000000"),               // 1 USDT
-                amountInMin: BigInt("980000000000000000"),              // 0.98 USDC
+                amountIn: BigInt("1000000000000000000"),               // 1 USDT
+                amountOutMin: BigInt("980000000000000000"),              // 0.98 USDC
                 swapPath: [
                     "0xf817257fed379853cDe0fa4F97AB987181B1E5Ea",
                     "0x88b8E2161DEDC77httEF4ab7585569D2415a1C1055D",
@@ -33,3 +33,6 @@ export const redeemDelegationController = async (req: Request, res: Response): P
         });
     }
 };
+// TODO : make a offchain blockchain pair checker
+// TODO : make an offchain blockchain swap path checker
+// TODO: make custom tokens,deploy the tokens,and make it liquid
